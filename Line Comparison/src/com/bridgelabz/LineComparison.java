05c824b72df4cbeae833bab960267fb1e26bb840
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-12 15:17:05 +0530

    Added : Both Lines Equal OR Not

--- Line Comparison/src/com/bridgelabz/LineComparison.java
+++ Line Comparison/src/com/bridgelabz/LineComparison.java
@@ -6,20 +6,41 @@ public class LineComparison {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
 
-        System.out.println("Enter value for x1");
-        int x1=sc.nextInt();
+        double sum1=0 , sum2=0;
 
-        System.out.println("Enter value for x2");
-        int x2=sc.nextInt();
+        for (int i = 1; i <= 2 ; i++) {
+            System.out.println("Enter the value for Line "+ i);
+            System.out.print("Enter value of x1 = ");
+            int x1 = sc.nextInt();
 
-        System.out.println("Enter value for y1");
-        int y1=sc.nextInt();
+            System.out.print("Enter value of x2 = ");
+            int x2 = sc.nextInt();
 
-        System.out.println("Enter value for y2");
-        int y2=sc.nextInt();
+            System.out.print("Enter value of y1 = ");
+            int y1 = sc.nextInt();
 
-        double length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-        System.out.println("Length of Line : " + length);
+            System.out.print("Enter value of y2 = ");
+            int y2 = sc.nextInt();
+
+
+            if (i==1)
+            {
+                sum1 =Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+            }
+            else{
+                sum2 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+
+            }
+        }
+        String sumofLine1= String.valueOf(sum1);
+        String sumofLine2= String.valueOf(sum2);
+        if (sumofLine1.equals(sumofLine2)) {
+            System.out.println("Both lines are equal");
+        }
+        else
+        {
+            System.out.println("Both lines are not equal");
+        }
 
     }
 }
