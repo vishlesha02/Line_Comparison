2a8c87b1084004521a7abdeb22f6e75a89878ed7
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-12 16:25:12 +0530

    Added : Compare Two Lines

--- Line Comparison/src/com/bridgelabz/LineComparison.java
+++ Line Comparison/src/com/bridgelabz/LineComparison.java
@@ -6,10 +6,11 @@ public class LineComparison {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
 
-        double sum1=0 , sum2=0;
+        Double sum1=0.0 , sum2=0.0;
 
-        for (int i = 1; i <= 2 ; i++) {
-            System.out.println("Enter the value for Line "+ i);
+        for (int i = 1; i <= 2; i++) {
+
+            System.out.println("Enter the value for Line " + i);
             System.out.print("Enter value of x1 = ");
             int x1 = sc.nextInt();
 
@@ -23,23 +24,25 @@ public class LineComparison {
             int y2 = sc.nextInt();
 
 
-            if (i==1)
-            {
-                sum1 =Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-            }
-            else{
-                sum2 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+            if (i == 1) {
+                sum1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+                System.out.println("Sum of line 1 :" + sum1);
+            } else {
+                sum2 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+                System.out.println("Sum of line 2 :" + sum2);
+
 
             }
         }
-        String sumofLine1= String.valueOf(sum1);
-        String sumofLine2= String.valueOf(sum2);
-        if (sumofLine1.equals(sumofLine2)) {
+
+        int comparison = sum1.compareTo(sum2);
+
+        if (comparison == 0) {
             System.out.println("Both lines are equal");
-        }
-        else
-        {
-            System.out.println("Both lines are not equal");
+        } else if (comparison > 0) {
+            System.out.println("Line 1 is greater than Line 2");
+        } else {
+            System.out.println("Line 2 is greater than Line 1");
         }
 
     }
